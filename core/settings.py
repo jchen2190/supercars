771d998abe199a12ff666e19a44572cbf2fbcfc7
Jon Chen <jchen2190@gmail.com>
2023-06-09 15:22:32 -0400

    change back endpoint

--- core/settings.py
+++ core/settings.py
@@ -150,8 +150,8 @@ if USE_SPACES:
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_DEFAULT_ACL = 'public-read'
-    # AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
-    AWS_S3_ENDPOINT_URL = 'https://supercars.nyc3.cdn.digitaloceanspaces.com'
+    AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
+    # AWS_S3_ENDPOINT_URL = 'https://supercars.nyc3.cdn.digitaloceanspaces.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
